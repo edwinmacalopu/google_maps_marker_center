4f9ad51d2d8fe75f63ce1d66aabe3e2e6f38c030
edwinmacalopu <khiwendz@gmail.com>
2022-05-23 22:53:51 -0500

    update flutter 3.0.1 :fire:

--- lib/src/bloc/bloc.dart
+++ lib/src/bloc/bloc.dart
@@ -1,52 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geocoding/geocoding.dart';
 
-class ProviderMaps with ChangeNotifier{
- LatLng _gpsactual;
- LatLng _initialposition=LatLng(-12.122711,-77.027475);
- bool activegps=true;
- TextEditingController locationController = TextEditingController();
- GoogleMapController _mapController;
-LatLng get gpsPosition => _gpsactual;
-LatLng get initialPos =>_initialposition;
-final Set<Marker> _markers=Set();
-Set<Marker> get markers => _markers;
-GoogleMapController get mapController => _mapController;
+class ProviderMaps with ChangeNotifier {
+  late LatLng _gpsactual;
+  LatLng _initialposition = LatLng(-12.122711, -77.027475);
+  bool activegps = true;
+  TextEditingController locationController = TextEditingController();
+  late GoogleMapController _mapController;
+  LatLng get gpsPosition => _gpsactual;
+  LatLng get initialPos => _initialposition;
+  final Set<Marker> _markers = Set();
+  Set<Marker> get markers => _markers;
+  GoogleMapController get mapController => _mapController;
 
-void getMoveCamera()async{
-  List<Placemark> placemark = await Geolocator()
-  .placemarkFromCoordinates(_initialposition.latitude, _initialposition.longitude); 
-   locationController.text = placemark[0].name;
-
-}
-
-void getUserLocation() async {
-  if (!(await Geolocator().isLocationServiceEnabled())) 
-    {     
-    activegps=false;     
-    }else{
-  activegps=true;               
-    Position position = await Geolocator()
-     .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    List<Placemark> placemark = await Geolocator()
-     .placemarkFromCoordinates(position.latitude, position.longitude);
-    _initialposition = LatLng(position.latitude, position.longitude);
-    print("the latitude is: ${position.longitude} and th longitude is: ${position.longitude} ");
-   locationController.text = placemark[0].name;
-    _addMarker(_initialposition,placemark[0].name);
-   _mapController.moveCamera(CameraUpdate.newLatLng(_initialposition));    
-    print("initial position is : ${placemark[0].name}");  
-    notifyListeners();
+  void getMoveCamera() async {
+    List<Placemark> placemark = await placemarkFromCoordinates(
+        _initialposition.latitude, _initialposition.longitude,
+        localeIdentifier: "en_US");
+    locationController.text = placemark[0].name!;
   }
-   }      
+
+  void getUserLocation() async {
+    if (!(await Geolocator.isLocationServiceEnabled())) {
+      activegps = false;
+    } else {
+      activegps = true;
+      LocationPermission permission;
+      permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          return Future.error('Location permissions are denied');
+        }
+      }
+
+      if (permission == LocationPermission.deniedForever) {
+        // Permissions are denied forever, handle appropriately.
+        return Future.error(
+            'Location permissions are permanently denied, we cannot request permissions.');
+      }
+
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      List<Placemark> placemark =
+          await placemarkFromCoordinates(position.latitude, position.longitude);
+      _initialposition = LatLng(position.latitude, position.longitude);
+      print(
+          "the latitude is: ${position.longitude} and th longitude is: ${position.longitude} ");
+      locationController.text = placemark[0].name!;
+      _addMarker(_initialposition, placemark[0].name!);
+      _mapController.moveCamera(CameraUpdate.newLatLng(_initialposition));
+      print("initial position is : ${placemark[0].name}");
+      notifyListeners();
+    }
+  }
+
   void onCreated(GoogleMapController controller) {
     _mapController = controller;
     notifyListeners();
   }
 
   void _addMarker(LatLng location, String address) {
-        _markers.add(Marker(
+    _markers.add(Marker(
         markerId: MarkerId(location.toString()),
         position: location,
         infoWindow: InfoWindow(title: address, snippet: "go here"),
@@ -54,9 +71,9 @@ void getUserLocation() async {
     notifyListeners();
   }
 
-  void onCameraMove(CameraPosition position)async {
+  void onCameraMove(CameraPosition position) async {
+    print(position.target);
     _initialposition = position.target;
     notifyListeners();
   }
-
 }
--- lib/src/page/Gmap_page.dart
+++ lib/src/page/Gmap_page.dart
@@ -1,133 +1,143 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:google_maps_marker_center/src/bloc/bloc.dart'; 
-import 'package:provider/provider.dart'; 
+import 'package:google_maps_marker_center/src/bloc/bloc.dart';
+import 'package:provider/provider.dart';
 
 class Gmap extends StatefulWidget {
-  Gmap({Key key}) : super(key: key);
+  Gmap({Key? key}) : super(key: key);
 
   @override
   _GmapState createState() => _GmapState();
 }
 
 class _GmapState extends State<Gmap> {
-    
   @override
   Widget build(BuildContext context) {
-    final provmaps=Provider.of<ProviderMaps>(context);
-   return provmaps.activegps==false?
-    Scaffold(
-      body:Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.symmetric(horizontal:50),
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                height: 250,
-                width: 250,
-                child: Image.asset('assets/images/nogps.png'),
-              ),
-              SizedBox(
-                height: 20,
-              )
-              ,Text('You must activate GPS to get your location',textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),),
-              SizedBox(
-                height:40,
-              ),
-              RaisedButton(onPressed: (){
-                provmaps.getUserLocation();
-              },
-              child: Text('try again'),
-              )
-            ],
-          ),
-        ),
-      )
-    ) 
-   : Scaffold(
-      body:SafeArea(
-              child: Stack(
-          alignment: Alignment.center,
-            children: <Widget>[
-          Positioned(
-          top: 0,
-             child: Container(
+    final provmaps = Provider.of<ProviderMaps>(context);
+    return provmaps.activegps == false
+        ? Scaffold(
+            body: Container(
             height: MediaQuery.of(context).size.height,
             width: MediaQuery.of(context).size.width,
-               child: GoogleMap(
-              zoomControlsEnabled: false,
-              mapType: MapType.normal,
-              //markers:provmaps.markers,
-              onCameraMove: provmaps.onCameraMove,
-              initialCameraPosition: CameraPosition(
-              target: provmaps.initialPos, zoom: 18.0),
-              onMapCreated:provmaps.onCreated,
-              onCameraIdle:()async{
-                provmaps.getMoveCamera();
-              } ,
-            ),
-          ),
-        ),
-        Positioned(
-        bottom: 20,
-        right: 20,
-        child:   FloatingActionButton(
-            onPressed: provmaps.getUserLocation,
-            backgroundColor: Colors.blueAccent,
-            child: Icon(Icons.gps_fixed,color: Colors.white,),
-          ),
-        ) ,
-        /*,*/
-    Positioned(
-        top: 0,
-        child: Container(
-          color: Colors.white,
-          height: 150,
-          width: MediaQuery.of(context).size.width,
-                child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
+            padding: EdgeInsets.symmetric(horizontal: 50),
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    height: 250,
+                    width: 250,
+                    child: Image.asset('assets/images/nogps.png'),
+                  ),
                   SizedBox(
-                      height: 20,
+                    height: 20,
                   ),
-                    Text("Google Maps",style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),),
-                     SizedBox(
-                      height: 30,
+                  Text(
+                    'You must activate GPS to get your location',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.bold),
                   ),
-                    TextField(
-                     maxLines: 1,
-              controller: provmaps.locationController,
-                    decoration: InputDecoration(
-                    prefixIcon:Icon(Icons.map),                     
-                    //hintText: 'CoNstr@seña',
-                    border:OutlineInputBorder(
-                    borderSide: BorderSide(
-                    color:Colors.white
+                  SizedBox(
+                    height: 40,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      provmaps.getUserLocation();
+                    },
+                    child: Text('try again'),
+                  )
+                ],
+              ),
+            ),
+          ))
+        : Scaffold(
+            body: SafeArea(
+              child: Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+                  Positioned(
+                    top: 0,
+                    child: Container(
+                      height: MediaQuery.of(context).size.height,
+                      width: MediaQuery.of(context).size.width,
+                      child: GoogleMap(
+                        zoomControlsEnabled: false,
+                        mapType: MapType.normal,
+                        markers: provmaps.markers,
+                        onCameraMove: provmaps.onCameraMove,
+                        initialCameraPosition: CameraPosition(
+                            target: provmaps.initialPos, zoom: 18.0),
+                        onMapCreated: provmaps.onCreated,
+                        onCameraIdle: () async {
+                          provmaps.getMoveCamera();
+                        },
+                      ),
                     ),
-                     borderRadius: BorderRadius.circular(10)
-                     )
-                   ), 
-                 ),
-                 ],
                   ),
-                )
-        ))
-        ,Align(
-                  alignment: Alignment.center,
-                  child: Icon(Icons.location_on,size: 50,color: Colors.redAccent,),
-                )
-            ],
-        ),
-      ),
-   
-    );   
-  } 
- 
+                  Positioned(
+                    bottom: 20,
+                    right: 20,
+                    child: FloatingActionButton(
+                      onPressed: provmaps.getUserLocation,
+                      backgroundColor: Colors.blueAccent,
+                      child: Icon(
+                        Icons.gps_fixed,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  /*,*/
+                  Positioned(
+                      top: 0,
+                      child: Container(
+                          color: Colors.white,
+                          height: 150,
+                          width: MediaQuery.of(context).size.width,
+                          child: Container(
+                            padding: EdgeInsets.symmetric(horizontal: 20),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Text(
+                                  "Google Maps",
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(
+                                  height: 30,
+                                ),
+                                TextField(
+                                  maxLines: 1,
+                                  controller: provmaps.locationController,
+                                  decoration: InputDecoration(
+                                      prefixIcon: Icon(Icons.map),
+                                      //hintText: 'CoNstr@seña',
+                                      border: OutlineInputBorder(
+                                          borderSide:
+                                              BorderSide(color: Colors.white),
+                                          borderRadius:
+                                              BorderRadius.circular(10))),
+                                ),
+                              ],
+                            ),
+                          ))),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Icon(
+                      Icons.location_on,
+                      size: 50,
+                      color: Colors.redAccent,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+  }
 }
